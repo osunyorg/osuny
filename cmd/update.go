0a722154d72b8d65a2b6b6aa7e2790a2cbde4d22
Arnaud Levy <contact@arnaudlevy.com>
2024-07-02 12:37:57 +0200

    test

--- cmd/update.go
+++ cmd/update.go
@@ -5,8 +5,6 @@ import (
 	"log"
 
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/config"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/spf13/cobra"
 )
 
@@ -17,7 +15,7 @@ var updateCmd = &cobra.Command{
 It is loaded as a submodule in themes/osuny.`,
 	Run: func(cmd *cobra.Command, args []string) {
 
-		branch := "main"
+		// branch := "main"
 
 		repository, err := git.PlainOpen(".")
 		if err != nil {
@@ -32,10 +30,13 @@ It is loaded as a submodule in themes/osuny.`,
 		}
 		err = worktree.Pull(&git.PullOptions{
 			RemoteName:        "origin",
+			ReferenceName:     "refs/heads/main",
+			SingleBranch:      true,
 			RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+			Force:             true,
 		})
 		if err != nil && err != git.NoErrAlreadyUpToDate {
-			fmt.Println(err)
+			fmt.Println("Pulled: %s", err)
 		}
 
 		submodules, err := worktree.Submodules()
@@ -50,65 +51,16 @@ It is loaded as a submodule in themes/osuny.`,
 				log.Fatalf("Error while getting submodule: %s", err)
 			}
 
-			err = subRepo.Fetch(&git.FetchOptions{
-				RemoteName: "origin",
-			})
-			if err != nil && err != git.NoErrAlreadyUpToDate {
-				log.Fatalf("Error while fetching submodule: %s", err)
-			}
-
 			w, err := subRepo.Worktree()
 			if err != nil {
 				log.Fatalf("Error getting submodule worktree: %s", err)
 			}
 
-			branchRefName := plumbing.NewBranchReferenceName(branch)
-			branchCoOpts := git.CheckoutOptions{
-				Branch: plumbing.ReferenceName(branchRefName),
-				Force:  true,
-			}
-
-			if err := w.Checkout(&branchCoOpts); err != nil {
-				// Warning("local checkout of branch '%s' failed, will attempt to fetch remote branch of same name.", branch)
-				// Warning("like `git checkout <branch>` defaulting to `git checkout -b <branch> --track <remote>/<branch>`")
-
-				mirrorRemoteBranchRefSpec := fmt.Sprintf("refs/heads/%s:refs/remotes/origin/%s", branch, branch)
-				err = fetchOrigin(subRepo, mirrorRemoteBranchRefSpec)
-				if err != nil {
-					log.Fatalf("Error fetching origin: %s", err)
-				}
-
-				err = w.Checkout(&branchCoOpts)
-				if err != nil {
-					log.Fatalf("Error fetching origin: %s", err)
-				}
-			}
+			w.Checkout(&git.CheckoutOptions{
+				Branch: "refs/heads/main",
+			})
 		}
 	},
-}
-
-func fetchOrigin(repo *git.Repository, refSpecStr string) error {
-	remote, err := repo.Remote("origin")
-	if err != nil {
-		log.Fatalf("Error getting remote origin: %s", err)
-	}
-
-	var refSpecs []config.RefSpec
-	if refSpecStr != "" {
-		refSpecs = []config.RefSpec{config.RefSpec(refSpecStr)}
-	}
-
-	if err = remote.Fetch(&git.FetchOptions{
-		RefSpecs: refSpecs,
-	}); err != nil {
-		if err == git.NoErrAlreadyUpToDate {
-			fmt.Print("refs already up to date")
-		} else {
-			return fmt.Errorf("fetch origin failed: %v", err)
-		}
-	}
-
-	return nil
 }
 
 func init() {
